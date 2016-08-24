51b96ad75b8fe1799b828724ae373ed6bb323f2c
Peter Halliday <pghalliday@gmail.com>
2016-08-24 14:39:42 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # setup-vm
+
 Script to initialize a new VM the way I like it
+
+## Usage
+
+Assuming the new VM is running an SSH server and is NAT'ed with the SSH port forwarded
+
+```
+./setup-vm.sh NAME USER SSH_PORT
+```
+
+This will
+
+- create an entry called NAME in the local SSH config
+- upload `~/.ssh/id_rsa` to the VM
+- upload `~/.ssh/id_rsa.pub` to the VM and set it as the `authorized_keys` file
+- copy SSH config to root user
+- Install the latest version of `git`
+- Set up dot files using https://github.com/pghalliday-dotfiles/scripts
+
+Currently supports the following distributions
+
+- Ubuntu
---
+++ setup-vm.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+set -e
+
+name=$1
+user=$2
+port=$3
+
+SSH=~/.ssh
+ID_RSA=$SSH/id_rsa
+ID_RSA_PUB=$SSH/id_rsa.pub
+CONFIG=$SSH/config
+
+USER_HOME=/home/$user
+USER_SSH=$USER_HOME/.ssh
+USER_ID_RSA=$USER_SSH/id_rsa
+USER_ID_RSA_PUB=$USER_SSH/id_rsa.pub
+USER_AUTHORIZED_KEYS=$USER_SSH/authorized_keys
+USER_KNOWN_HOSTS=$USER_SSH/known_hosts
+
+ROOT_HOME=/root
+ROOT_SSH=$ROOT_HOME/.ssh
+ROOT_ID_RSA=$ROOT_SSH/id_rsa
+ROOT_ID_RSA_PUB=$ROOT_SSH/id_rsa.pub
+ROOT_AUTHORIZED_KEYS=$ROOT_SSH/authorized_keys
+ROOT_KNOWN_HOSTS=$ROOT_SSH/known_hosts
+
+PROJECTS=$USER_HOME/projects
+DOTFILES_SCRIPTS_REPO=git@github.com:pghalliday-dotfiles/scripts.git
+DOTFILES=$PROJECTS/pghalliday-dotfiles
+DOTFILES_SCRIPTS=$DOTFILES/scripts
+TERMINAL_SETUP=$DOTFILES_SCRIPTS/terminal-setup.sh
+
+####################
+# Local SSH config #
+####################
+
+# Add the SSH config entry
+mkdir -p $SSH
+chmod 0700 $SSH
+cat >> $CONFIG << END_CONFIG
+
+Host $name
+  HostName localhost
+  User $user
+  Port $port
+END_CONFIG
+
+#################################
+# VM SSH configuration for user #
+#################################
+
+# Configure VM SSH keys (will prompt for password)
+ssh $name << --END_SCRIPT--
+mkdir -p $USER_SSH
+chmod 0700 $USER_SSH
+cat > $USER_ID_RSA << --END_PRIVATE_KEY--
+$(cat $ID_RSA)
+--END_PRIVATE_KEY--
+chmod 0600 $USER_ID_RSA
+cat > $USER_ID_RSA_PUB << --END_PUBLIC_KEY--
+$(cat $ID_RSA_PUB)
+--END_PUBLIC_KEY--
+cp $USER_ID_RSA_PUB $USER_AUTHORIZED_KEYS
+chmod 0600 $USER_AUTHORIZED_KEYS
+--END_SCRIPT--
+
+# Set sudo to be passwordless for user (will  prompt for password)
+ssh -t $name "sudo sh -c \"echo '$user ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$user\""
+
+###########################################################
+# Setup known hosts here before copying SSH setup to root #
+###########################################################
+
+# Required for cloning from github without prompt
+ssh $name "ssh-keyscan github.com > $USER_KNOWN_HOSTS"
+
+###################################
+# copy SSH setup to the root user #
+###################################
+
+ssh $name << END_SCRIPT
+sudo sh << END_ROOT_SCRIPT
+mkdir -p $ROOT_SSH
+chmod 0700 $ROOT_SSH
+cp $USER_ID_RSA $ROOT_ID_RSA
+cp $USER_ID_RSA_PUB $ROOT_ID_RSA_PUB
+cp $USER_AUTHORIZED_KEYS $ROOT_AUTHORIZED_KEYS
+cp $USER_KNOWN_HOSTS $ROOT_KNOWN_HOSTS
+END_ROOT_SCRIPT
+END_SCRIPT
+
+###############################
+# Distribution specific stuff #
+###############################
+
+if ssh $name "hash add-apt-repository 2>/dev/null"; then
+  # Ubuntu
+
+  # Install latest git
+  ssh $name "sudo add-apt-repository ppa:git-core/ppa -y && sudo apt-get update -y && sudo apt-get install git -y"
+else
+  echo "Unsupported distribution!"
+  exit 1
+fi
+
+##################
+# Setup dotfiles #
+##################
+
+ssh $name "git clone $DOTFILES_SCRIPTS_REPO $DOTFILES_SCRIPTS && $TERMINAL_SETUP"
